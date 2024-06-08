eb7c105054441733945a860ef9d7737770a84410
Costantin777 <145683428+Costantin777@users.noreply.github.com>
2024-06-08 20:27:37 +0300

    new

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,17 +1,21 @@
 import fs from 'fs/promises';
+import path from 'path';
 import { PATH_DB } from '../constants/contacts.js';
 
 export const thanos = async () => {
   try {
+    // Складаємо шлях до файлу з контактами
+    const filePath = path.resolve(PATH_DB);
+
     // Читаємо існуючі контакти з файлу
-    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const data = await fs.readFile(filePath, 'utf-8');
     let contacts = JSON.parse(data);
 
     // Проходимо по кожному контакту та з імовірністю 50% видаляємо його
     contacts = contacts.filter(() => Math.random() > 0.5);
 
     // Записуємо оновлений масив назад у файл
-    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+    await fs.writeFile(filePath, JSON.stringify(contacts, null, 2));
 
     console.log('Success');
   } catch (error) {
